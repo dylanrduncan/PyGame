decb51fd49d91ff2ceb4ef3202f3e33e88a7e18e
Dylan Duncan <Dylan_Duncan1@baylor.edu>
2021-03-14 23:58:38 -0500

    sped up bullets, added alien/ship collision detection

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,11 +75,24 @@ class AlienInvasion:
         """Update position of bullets and get rid of old bullets."""
         # update bullet positions.
         self.bullets.update()
-
+        # Check for any bullets that have hhit aliens.
+        # If so, get rid of the bullet and the alien.
         # Get rid of bullets that have dissappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        self._check_bullet_alien_colisions()
+
+    def _check_bullet_alien_colisions(self):
+        """respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # destory existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_screen(self):
         """Update images on the screen, and flip to new screen"""
@@ -128,6 +141,10 @@ class AlienInvasion:
         self._check_fleet_edges()
         """update the positions of all aliens in the fleet."""
         self.aliens.update()
+
+        # look for alien-ship collisions
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
 
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
